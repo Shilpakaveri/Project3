b053f8d86ffa0ebf3a23e202973214c6c5c2c14c
Shilpakaveri <shilpakaveri230@gmail.com>
2023-06-19 11:12:00 +0530

    first

---
+++ calapp/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+def calculate(request):
+    x=int(request.POST["t1"])
+    y=int(request.POST["t2"])
+    op=request.POST["op"]
+    res=0
+    data=""
+    if op=="add":
+        res=x+y
+        data="The sum is:"
+    elif op=="sub":
+        res=x-y
+        data="The sub is:"
+    elif op=="mul":
+        res=x*y
+        data="The mul is:"
+    else:
+        res=x/y
+        data="The division is:"
+    return HttpResponse(data+str(res))
